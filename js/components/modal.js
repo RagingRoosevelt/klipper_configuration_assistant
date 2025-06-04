0ff42e61b18ebe55200c694a1d31fc56365889f0
RagingRoosevelt <theodore.lindsey@outlook.com>
2025-06-04 03:48:33 -0700

    Add in-progress toast notification

--- js/components/modal.js
+++ js/components/modal.js
@@ -1,7 +1,7 @@
 import van from "../frameworks/van-1.5.5.js"
 const { button, code, dialog, div, modal } = van.tags
 
-export const Modal = (show, modal_info) => {
+export const Modal = (show, modal_info, toast_info) => {
   return dialog(
     {open: van.derive(()=>show.val),style: "flex-direction: column"},
     div(
@@ -11,7 +11,13 @@ export const Modal = (show, modal_info) => {
       )
     ),
     div(
-      button({onclick: ()=>navigator.clipboard.writeText(modal_info.val.body)}, "Copy"),
+      button({onclick: ()=>{
+        toast_info.val = {
+          message: "Copied to clipboard",
+          type: "info",
+        }
+        navigator.clipboard.writeText(modal_info.val.body)
+      }}, "Copy"),
       button({onclick: ()=>show.val = false}, "Close")
     )
   )
